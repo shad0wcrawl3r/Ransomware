1930977e33a516c836bb97eef695f3485f6d8426
Ashwin Belbase <46002059+shad0wcrawl3r@users.noreply.github.com>
2021-05-06 20:54:41 +0545

    Updated to read from list of encrypted

--- server/decrypt.py
+++ server/decrypt.py
@@ -5,11 +5,16 @@ def decrypt():
 	    symkey=kf.read()
 
 	fern = Fernet(symkey)
-	with open('test.pdf','rb') as tf:
-	    crypto=fern.decrypt(tf.read())
+	with open ('list_of_encrypted.plain','r') as encf:
+		list_of_enc=encf.read().split('\n')
+		lsit_of_enc.pop()
+	
+	for each in list_of_enc:
+		with open(each+".enc",'rb') as tf:
+		    crypto=fern.decrypt(tf.read())
 
-	with open('test.pdf','wb') as tf:
-	    tf.write(crypto)
+		with open(each,'wb') as tf:
+		    tf.write(crypto)
 
 
 
